df4781bea1fa1d72a26b8e3abe44897e3777939d
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-09-23 14:06:11 +0100

    to do list completed and operative challenge completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [listOfItems, setItems] = useState([]);
+
+  function handleInputText(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function handleAddButton() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input
+          type="text"
+          onChange={handleInputText}
+          value={inputText}
+          placeholder="New Task"
+        />
         <button>
-          <span>Add</span>
+          <span onClick={handleAddButton}>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {listOfItems.map((toDoItem) => (
+            <li>{toDoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
